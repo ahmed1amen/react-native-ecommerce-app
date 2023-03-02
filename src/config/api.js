21444172a50d59b990045aeef1f49604a1bde90b
AhmedAmen <ahmed.amen@refilex.com>
2023-03-02 15:45:08 +0200

    git init

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+export const API = 'https://kenziekids.net';
+
+export const CONSUMER_KEY = 'ck_61feaf47c2cdf4a861754586fd833ad59cb890c5';
+export const CONSUMER_SECRET = 'cs_b3300c461bd4f0fe0a7e6b7ee01f7f30b8cbfc0e';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+};
---
+++ src/config/auth.js
@@ -0,0 +1,8 @@
+export const GOOGLE_SIGN_IN_CONFIG = {
+  webClientId:
+    '272520955164-1l9agu08imb8iisova65fm5pfavg3p3o.apps.googleusercontent.com',
+  offlineAccess: true,
+};
+
+export const SUPPORT_DIGITS_PLUGIN = false;
+
